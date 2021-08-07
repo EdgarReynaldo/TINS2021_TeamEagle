5861362c4bd4fc83575a86d6ebd20ff7ffd6aed7
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2021-08-07 16:55:25 -0500

    Basic initial commit

---
+++ Hexagoner.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+#include "Eagle/backends/Allegro5Backend.hpp"
+#include "Eagle.hpp"
+#include "allegro5/allegro.h"
+
+
+
+
+
+int main(int argc , char** argv) {
+   
+   
+   
+   
+   return 0;
+};
---
+++ README.txt
@@ -0,0 +1,14 @@
+
+
+
+
+Hexagoner
+By Marc Davenport and Brandon Mccaig for TINS2021
+
+Code Reuse
+
+License
+
+Libraries used :
+Eagle 0pt8pt2
+Allegro 5.2.7.1GIT
