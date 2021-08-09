307c90d5570b850e6605c8306659feb53cd5c42b
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2021-08-09 09:37:06 -0500

    Added some code from LibKrampus2018

--- include/Hexagon3D.hpp
+++ include/Hexagon3D.hpp
@@ -13,7 +13,7 @@ class Hexagon3D {
    
    std::vector<ALLEGRO_VERTEX> verts;
    std::vector<ALLEGRO_VERTEX> faces;
-   std::vector<ALLEGRO_VERTEX> triangles;
+///   std::vector<ALLEGRO_VERTEX> triangles;
    
 public :
    
@@ -24,8 +24,8 @@ public :
       outer(0.0),
       depth(0.0),
       verts(4*6 , ALLEGRO_VERTEX()),/// [4 rings][6 vertices]
-      faces(4*6*4 , ALLEGRO_VERTEX()),/// [4 sides][6 faces][4 vertices]
-      triangles(4*6*5 , ALLEGRO_VERTEX())/// [4 sides][6 faces][5 vertices] (triangle fan)
+      faces(4*6*4 , ALLEGRO_VERTEX())/// [6 sides][4 faces][4 vertices]
+///      triangles(4*6*5 , ALLEGRO_VERTEX())/// [6 sides][4 faces][5 vertices] (triangle fan)
    {
       BuildHexagon(iradius , oradius , zdepth);
    }
@@ -44,25 +44,41 @@ public :
       std::vector ov = ho.GetVerts();
       for (unsigned int vertex = 0 ; vertex < 6 ; ++vertex) {
          ALLEGRO_VERTEX v1,v2,v3,v4,vi,vo;
+         
+         ALLEGRO_COLOR c = al_map_rgb((vertex/2 % 3 == 0)?255:0 , (vertex/2 % 3 == 1)?255:0 , (vertex/2 % 3 == 2)?255:0);
+         
+         
+         
          vi = iv[vertex];
          vo = ov[vertex];
          v1 = vi;
          v2 = vi;
          v3 = vo;
          v4 = vo;
-         v1.z = -depth/2.0;
-         v2.z = depth/2.0;
-         v3.z = -depth/2.0;
-         v4.z = depth/2.0;
+         v1.z = -depth/2.0;// Inner lower
+         v2.z = depth/2.0;// Inner upper
+         v3.z = depth/2.0;// Outer upper
+         v4.z = -depth/2.0;// Outer lower
          verts[4*vertex + 0] = v1;
          verts[4*vertex + 1] = v2;
          verts[4*vertex + 2] = v3;
          verts[4*vertex + 3] = v4;
       }
+      for (unsigned int vertex = 0 ; vertex < 6 ; ++vertex) {
+         for (unsigned int side = 0 ; side < 4 ; ++side) {
+            faces[side*24 + vertex*4 + 0] = verts[4*((vertex + 0)%6) + ((side + 0)%4];
+            faces[side*24 + vertex*4 + 1] = verts[4*((vertex + 0)%6) + ((side + 1)%4];
+            faces[side*24 + vertex*4 + 2] = verts[4*((vertex + 1)%6) + ((side + 1)%4];
+            faces[side*24 + vertex*4 + 3] = verts[4*((vertex + 1)%6) + ((side + 0)%4];
+         }
+      }
    }
    
       
-   
+   void DrawHexagon();
+   void DrawHexagon() {
+      
+   }
    
    
    
